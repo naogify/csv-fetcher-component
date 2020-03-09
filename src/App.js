90014f9df67f25e96ee87cea8e14b641c3bd79ac
Naoki Ohashi <n.globe.us@gmail.com>
2020-03-09 23:59:51 +0900

    Add csv fetch function

--- src/App.js
+++ src/App.js
@@ -2,10 +2,26 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
+  let url =
+    "https://raw.githubusercontent.com/wakayama-pref-org/road-regulation-information_abnormal-weather/master/CSV/Road-regulation-information_Abnormal-weather.csv";
+  fetch(url)
+    .then(response => response.text())
+    .then(transform);
   return (
     <div className="App">
-      <h1>Helllllooo CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Component to Fetch CSV.</h1>
+      <h2>You can see fetch result by opening debug tool.</h2>
+      <div />
     </div>
   );
+}
+export function transform(str) {
+  let data = str.split("\n").map(i => i.split(","));
+  let headers = data.shift();
+  let output = data.map(d => {
+    let obj = {};
+    headers.map((h, i) => (obj[headers[i]] = d[i]));
+    return obj;
+  });
+  console.log(output);
 }
